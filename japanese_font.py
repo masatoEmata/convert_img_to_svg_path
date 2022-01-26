824e0b7be941f916fba2bd35ba6dfe432998d140
masatoEmata <lh57.nm@gmail.com>
2022-01-26 18:28:16 +0900

    'addDescription'

--- japanese_font.py
+++ japanese_font.py
@@ -4,11 +4,11 @@ from dataclasses import dataclass
 
 @dataclass
 class JapaneseFont:
-    img: np.ndarray
-    text: str
-    org: tuple
+    img: np.ndarray  # 第一引数の値が小さすぎると文字描画領域が小さくなり文字が出力されなくなる。文字が収まる最小領域を確保できれば良い。
+    text: str  # 日本語OK。\nを使っての改行もOK。
+    org: tuple  # テキストの左上の座標を(x,y)で指定する。
     fontFace: str
-    fontScale: int
+    fontScale: int  # 「1」の大きさはフォントにより異なる。
     color: tuple
 
     def cv2_putText(self):
