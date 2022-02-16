e643823f0a5b51adb178f88a719d4c4b7e7cd65e
masatoEmata <lh57.nm@gmail.com>
2022-02-16 14:58:06 +0900

    'splitClass'

--- svg_letter_handler.py
+++ svg_letter_handler.py
@@ -1,20 +1,23 @@
 from curses.ascii import isspace
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple, Dict
 from svgpathtools import svg2paths2, wsvg, Path
 from svgpathtools import CubicBezier, Line, Arc
 
 
 @dataclass
-class SvgLetterMover:
+class SvgLetterReader:
     input_file_path: str
-    x_delta: float
-    y_delta: float
-    scaling: float
-
     def read_svg_letter(self):
         paths, attributes, svg_attributes = svg2paths2(self.input_file_path)
         return (paths, attributes, svg_attributes)
+
+
+@dataclass
+class SvgLetterMover:
+    x_delta: float
+    y_delta: float
+    scaling: float
 
     def __calc_move_point(self, complex_pair: complex, x_delta: float, y_delta: float, scaling: float):
         x, y = scaling * (complex_pair.real + x_delta), scaling * (complex_pair.imag + y_delta)
@@ -68,13 +71,23 @@ class SvgLetterMover:
                 combine_paths.append(elm)
         return Path(*combine_paths)
 
+    def __move_path(self, path: Path) -> Path:
+        moved_path = [self.__move_path_elm(elm) for elm in path]
+        return Path(*moved_path)
+
     def move_paths(self, paths: List[Path]) -> List:
         combined_path = self.__combine_paths(paths)
         return self.__move_path(combined_path)
 
-    def __move_path(self, path: Path) -> Path:
-        moved_path = [self.__move_path_elm(elm) for elm in path]
-        return Path(*moved_path)
+
+@dataclass
+class SvgLetterWriter:
+    moved_paths: List
+    svg_attributes: Dict
+    output_file_path: str
+
+    def write(self):
+        wsvg(self.moved_paths, svg_attributes=self.svg_attributes, filename=self.output_file_path)
 
 
 @dataclass
@@ -88,19 +101,25 @@ class Position:
 
 def main():
     def __init_position(index: int, letter_length: int, spacing: float) -> float:
-        return index * (letter_length * ( 1 + spacing))
+        return index * (letter_length * (1 + spacing))
 
-    def __move_charactor(input_file_path, x_delta, y_delta, scaling):
-        mover = SvgLetterMover(input_file_path, x_delta, y_delta, scaling)
-        svg_letter = mover.read_svg_letter()
-
+    def __load_charctors(input_file_path) -> Tuple:
+        reader = SvgLetterReader(input_file_path)
+        svg_letter = reader.read_svg_letter()
         paths = svg_letter[0]
         svg_attributes = svg_letter[2]
-        moved_paths = mover.move_paths(paths)
+        return (paths, svg_attributes)
 
+    def __move_paths(x_delta: float, y_delta: float, scaling: float, paths: List[Path]) -> List:
+        mover = SvgLetterMover(x_delta, y_delta, scaling)
+        return mover.move_paths(paths)
+
+    def __move_charactor(input_file_path: str, x_delta: float, y_delta: float, scaling: float) -> Tuple:
+        paths, svg_attributes = __load_charctors(input_file_path)
+        moved_paths = __move_paths(x_delta, y_delta, scaling, paths)
         return (moved_paths, svg_attributes)
 
-    def move_whole_charactor(passage, position: Position):
+    def move_whole_charactor(passage: str, position: Position) -> Tuple:
         moved_paths = []
         svg_attributes = {}
         for left_index, char in enumerate(passage):
@@ -123,8 +142,7 @@ def main():
     output_file_path = f'./data/output/svg/{output_file_title}.svg'
     position = Position(13, 13, 0.25, 0.5, 2)
     moved_paths, svg_attributes = move_whole_charactor(passage, position)
-    print(moved_paths)
-    wsvg(moved_paths, svg_attributes=svg_attributes, filename=output_file_path)
+    SvgLetterWriter(moved_paths, svg_attributes, output_file_path).write()
 
 
 if __name__ == '__main__':
